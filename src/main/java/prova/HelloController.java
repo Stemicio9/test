27e65b0df32e3277f0520d9846c2904cc2c125b6
stefanomiceli <s.miceli90@gmail.com>
2018-09-03 20:45:25 -0700

    Third commit

--- src/main/java/prova/HelloController.java
+++ src/main/java/prova/HelloController.java
@@ -1,6 +1,7 @@
 package prova;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(){
         return "Hello";
     }
